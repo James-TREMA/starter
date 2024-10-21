999d2637cc536ffdbb4d179b62cfbe6bded8d221
James-Trema <andrdu29@live.fr>
2024-10-21 14:55:03 +0200

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { IntroComponent } from './intro/intro.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { NgifComponent } from './ngif/ngif.component';
+import { NgforComponent } from './ngfor/ngfor.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'intro', component: IntroComponent },
+    { path: 'data-binding', component: DataBindingComponent },
+    { path: 'ngif', component: NgifComponent },
+    { path: 'ngfor', component: NgforComponent },
     { path: 'about/:id', component: AboutComponent }
 ];
