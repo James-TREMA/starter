bcfd3d2c20f843a1168d71eb6c0ff04f92cd759a
James-Trema <andrdu29@live.fr>
2024-10-21 14:24:59 +0200

    up

---
+++ src/app/ngif/ngif.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngif',
+  standalone: true,
+  imports: [],
+  templateUrl: './ngif.component.html',
+  styleUrl: './ngif.component.css'
+})
+export class NgifComponent {
+
+}
