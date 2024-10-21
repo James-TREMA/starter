31c8bc40839ea551b01d039e55c29ba2f4c6285c
James-Trema <andrdu29@live.fr>
2024-10-21 14:12:32 +0200

    up

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,15 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.css'
 })
 export class IntroComponent {
-  title: string = 'Introduction'
+  title: string = 'angular'
   visible: boolean = false
+
+  users = [
+    {id: 1, name: 'Pierre'},
+    {id: 2, name: 'Jeremy'},
+    {id: 3, name: 'Alain'},
+  ]
 }
