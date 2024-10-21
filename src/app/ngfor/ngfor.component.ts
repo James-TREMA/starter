e9859b35b7ecfbe6d93b5af39c7bd1965773b49e
James-Trema <andrdu29@live.fr>
2024-10-21 14:31:06 +0200

    up

--- src/app/ngfor/ngfor.component.ts
+++ src/app/ngfor/ngfor.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ngfor',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './ngfor.component.html',
   styleUrl: './ngfor.component.css'
 })
 export class NgforComponent {
+  title: string = 'angular'
+  visible: boolean = false
 
+  users = [
+    {id: 1, name: 'Pierre'},
+    {id: 2, name: 'Jeremy'},
+    {id: 3, name: 'Alain'},
+  ]
 }
--- src/app/ngif/ngif.component.ts
+++ src/app/ngif/ngif.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ngif',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './ngif.component.html',
   styleUrl: './ngif.component.css'
 })
 export class NgifComponent {
+  title: string = 'angular'
+  visible: boolean = false
 
+  users = [
+    {id: 1, name: 'Pierre'},
+    {id: 2, name: 'Jeremy'},
+    {id: 3, name: 'Alain'},
+  ]
 }
