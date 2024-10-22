c0d891630cd93d5b059978f973cd0cc8bfb45424
James-Trema <andrdu29@live.fr>
2024-10-22 10:05:45 +0200

    Ajout du components Login plus remplissage and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { AboutComponent } from './pages/about/about.component';
 import { DataBindingComponent } from './pages/data-binding/data-binding.component';
 import { NgifComponent } from './pages/ngif/ngif.component';
 import { NgforComponent } from './pages/ngfor/ngfor.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
+    { path: 'login', component: LoginComponent},
     { path: '', component: HomeComponent },
     { path: 'data-binding', component: DataBindingComponent },
     { path: 'ngif', component: NgifComponent },
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.minLength(3), Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      console.log(this.loginForm.value)
+    }
+  }
+
+  get email() { return this.loginForm.get('email') }
+  get password() { return this.loginForm.get('password') }
 }
